86bbe6cfd44740409c6eb21edd1e0858460900af
hmanhng <hmanhng@icloud.com>
2022-06-19 01:01:58 +0700

    up

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 if [[ -z $1 ]]
 then
+  stow dunst --adopt
   stow alacritty --adopt
   stow kitty --adopt
   stow zsh --adopt
   stow lvim --adopt
   git reset --hard
 else
-  stow -S $1 -S $2 -S $3 -S $4  --adopt
+  stow -S $1 -S $2 -S $3 -S $4 -S $5  --adopt
   git reset --hard
 fi
